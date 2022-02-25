415f07338442f17ca171a0b5b25da8aba79a66cd
Joud El-Shawa <jelshawa@uwo.ca>
2022-02-24 23:54:22 -0500

    new database on google cloud platform

--- DBConnection.js
+++ DBConnection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 async function newConnection()
 {
     let connection = await mysql.createConnection({
-        host: '104.198.24.39',
+        host: '35.222.11.233',
         user: 'root',
         password: 'JIcmozn2pdbFHwIa',
         database: 'bankDB'
